6d9316d707a9dde5ff5115080ad0e3a141db66ad
JavierPulidoT <javierp.pulido@gmail.com>
2022-10-28 15:31:15 -0500

    Desafío de código: Súbete a ese juego

---
+++ ProgrammingBasics_Level1/Pulido_Javier_JS_SubeteAeseJuego.js
@@ -0,0 +1,18 @@
+//Desafío de código: Súbete a ese juego
+
+//Altura Niño
+var altura_n = 55; //entrada
+
+
+function PuedeSubir(altura_n) {
+    //condicional
+    if (altura_n > 52) {
+        console.log("¡Súbete, chico!");
+    }
+    else {
+        console.log("Lo siento, chico. Tal vez el próximo año”");
+    }
+    
+}
+//Mando a llamar la function
+PuedeSubir();

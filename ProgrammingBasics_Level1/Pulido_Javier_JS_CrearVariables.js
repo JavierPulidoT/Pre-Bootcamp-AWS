169a5ff3ffd5400ecd071e26ea78680c86103d67
JavierPulidoT <javierp.pulido@gmail.com>
2022-10-28 14:43:07 -0500

    Ejercicio Montaña Rusa

---
+++ ProgrammingBasics_Level1/Pulido_Javier_JS_CrearVariables.js
@@ -0,0 +1,13 @@
+//Crear Variables , Ejercicio  "Montaña Rusa"
+
+//Declaracion de las variables, con datos Pre-definidos
+var altura =1;
+var edad = 12;
+
+// Condicional , el cual debe cumplir los criterios de ambaas variables 
+// en este caso la edad y la altura reglamentaria.
+if (altura =>1 & edad >10) {
+    console.log("Puede subirse a la montaña rusa")
+}else{
+    console.log("NO puede subirse a la montaña rusa")
+}

b6107002f09eb92b6c5c42490fe8b397040b2dc8
JavierPulidoT <javierp.pulido@gmail.com>
2022-10-29 14:21:32 -0500

    Desafío parametros, Count Dooku

---
+++ ProgrammingBasics_Level1/Pulido_Javier_DesafioCodigo_Parametros.js
@@ -0,0 +1,27 @@
+// Desafío de código: Parámetros
+
+function CountDooku(a) {
+        if (a == "Javier") {
+            console.log("Buenos dias");
+               }   
+                if (a == "5:00pm") {
+                console.log("Hora del saludo!" ,a);
+                   }
+
+            else {
+                console.log("¡Buenos dias: ",a);
+            }
+    
+        if (a == "Count Dooku") {
+            console.log("¡Voy por ti, Dooku!😈" );
+               }
+               
+    }
+
+    CountDooku("Javier");
+    CountDooku("5:00pm");
+    CountDooku("Count Dooku");
+
+  
+    
+    

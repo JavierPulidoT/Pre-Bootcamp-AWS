304898c0f6001403265456233d62631a336c2898
JavierPulidoT <javierp.pulido@gmail.com>
2022-10-28 22:04:44 -0500

    Depuración de codigo

---
+++ ProgrammingBasics_Level1/Pulido_Javier_Codigo_Depuracion.js
@@ -0,0 +1,14 @@
+//Depuración
+//Arreglando el siguiente codigo
+
+
+function greeting(){
+    return "Hola, mundo";
+}
+var word = greeting();
+console.log(word);  //word toma el valor de la funcion el cual el return le da el hola mundo
+
+
+
+
+//JavierPulido
